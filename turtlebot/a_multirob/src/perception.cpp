0f0c6e00293bdb519462f5fa19b00b5bd94078a0
José Miguel <36297047+jmtc7@users.noreply.github.com>
2019-04-24 20:00:36 +0200

    Add thresholding, bounding box and centroid
    Now the image callback converts the original frame to HSV, threshold (to find green parts), extract contours, find the biggest one, calculates the bounding rectangle and the centroid (using the moments) and draw this two last things in the original frame.

--- turtlebot/a_multirob/src/perception.cpp
+++ turtlebot/a_multirob/src/perception.cpp
@@ -5,23 +5,59 @@
 #include <cv_bridge/cv_bridge.h>
 #include "std_msgs/Float32.h"
 
-
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
 	try
 	{
+		//Convert from ROS image message to OpenCV image
+		cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
+
 		//Convert from BGR to HSV
-		//
+		cv::Mat img_hsv;
+		cv::cvtColor(img, img_hsv, cv::COLOR_BGR2HSV);
+
 		//Threshold to get only the green parts
-		//
+		cv::Mat thresholded_img;
+		inRange(img_hsv, cv::Scalar(20, 0, 0), cv::Scalar(80, 255, 255), thresholded_img);
+		
 		//Search contours 
-		//
-		//Find the biggest contour
-		//
-		//Calculate area, centroid and bounding box
-		//
+		std::vector<std::vector<cv::Point> > contours;
+		std::vector<cv::Vec4i> hierarchy;
+		findContours(thresholded_img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+
+		//Find the biggest contour (by measuring their bounded areas)
+		int biggest_idx = 0;
+		double biggest_area = 0.0;
+
+		for (int i=0; i<contours.size(); i++)
+		{
+			double current_area = cv::contourArea(contours[i]);
+
+			if (current_area > biggest_area)
+			{
+				biggest_idx = i;
+				biggest_area = current_area;
+			}
+		}
+
+		std::vector<cv::Point> biggest_cnt = contours[biggest_idx];
+
+		//Calculate bounding rectangle for the biggest area
+		cv::Rect bounding_box = cv::boundingRect(biggest_cnt);
+
+		//Calculate the centroid of the biggest area
+		cv::Moments mu = cv::moments(biggest_cnt);
+		cv::Point2f centroid = cv::Point2f(mu.m10/mu.m00, mu.m01/mu.m00 ); //[m10/m00; m01/m00]
+		
+		//Draw centroid and bounding box
+		cv::circle(img, centroid, 2, cv::Scalar(0, 0, 255), -1, 8, 0 ); //Centroid
+		cv::rectangle(img,
+				cv::Point(bounding_box.x, bounding_box.y),
+				cv::Point(bounding_box.x + bounding_box.width, bounding_box.y + bounding_box.height),
+				cv::Scalar(0, 0, 255)); //Bounding box
+		
 		//Show image (with centroid and bounding box)
-		cv::imshow("green_detection", cv_bridge::toCvShare(msg, "bgr8")->image);
+		cv::imshow("green_detection", img);
 		cv::waitKey(30);
 	}
 	catch (cv_bridge::Exception& e)
