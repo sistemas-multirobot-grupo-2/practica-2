a5a46e84188ae20ba4b475aed31d7c8417b94970
yoinelnovelotellez <41169356+yoinelnovelotellez@users.noreply.github.com>
2019-04-30 16:51:50 +0200

    Fixed compilation problems

--- planning/src/planning.cpp
+++ planning/src/planning.cpp
@@ -4,12 +4,12 @@
 #include <string>
 #include <sstream>
 #include "std_msgs/String.h"
+#include <move_base_msgs/MoveBaseAction.h>
 
 using namespace std;
 
 move_base_msgs::MoveBaseGoal position;
 
-ros::init(argc, argv, "planning");
 ros::NodeHandle n;
 // publisher definition
 ros::Publisher next_leader_position_pub = n.advertise<std_msgs::String>("next_leader_position", 1000);
@@ -17,38 +17,39 @@ ros::Rate loop_rate(1);
 
 
 void nextLeaderPositionCallback(const std_msgs::String::ConstPtr& msg)
-{ 
-  string message= msg->data.c_str();
+{
+  string message = msg->data.c_str();
   std::stringstream ss;
-  switch (message)
+  std_msgs::String msg2;
+  if(message == "home")
   {
-    case "home":
-      ss << "pose1";
-      msg->data = ss.str();
-      next_leader_position_pub.publish(msg);
-    break;
-    case "pose1":
-      ss << "pose2";
-      msg->data = ss.str();
-      next_leader_position_pub.publish(msg);
-    break;
-    case "pose2":
-      ss << "pose3";
-      msg->data = ss.str();
-      next_leader_position_pub.publish(msg);
-    break;
-    case "pose3":
-      ss << "home";
-      msg->data = ss.str();
-      next_leader_position_pub.publish(msg);
-    break;
-    default:
-    break;
+    ss << "pose1";
+    msg2.data = ss.str();
+    next_leader_position_pub.publish(msg);
+	}
+  else if(message == "pose1")
+  {
+    ss << "pose2";
+    msg2.data = ss.str();
+    next_leader_position_pub.publish(msg);
+  }
+  else if(message == "pose2")
+  {
+    ss << "pose3";
+    msg2.data = ss.str();
+    next_leader_position_pub.publish(msg);
+  }
+  else if(message == "pose3")
+  {
+    ss << "pose4";
+    msg2.data = ss.str();
+    next_leader_position_pub.publish(msg);
   }
 }
 
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "planning");
   // subscriber definition
   ros::Subscriber leader_position_sub = n.subscribe("leader_position", 1000, nextLeaderPositionCallback);
   while (ros::ok())
@@ -57,3 +58,4 @@ int main(int argc, char** argv)
     loop_rate.sleep();
   }
 }
+
